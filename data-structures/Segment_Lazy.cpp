efd61450e3ea6703ed4cd42f0aed47917ed044b4
Omar Adel <OmarAdel.connect@gmail.com>
2026-02-22 05:01:46 +0200

    Add Segment with Lazy

---
+++ data-structures/Segment_Lazy.cpp
@@ -0,0 +1,130 @@
+// بسم الله الرحمن الرحيم
+// وَأَنْ لَيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَىٰ
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define sz(x) x.size()
+
+struct Node
+{
+    int val, lazy = 0;
+    bool isLazy = false;
+
+    Node() : val(0) {}
+
+    Node(int x) : val(x) {}
+
+    void update(int v, int lx, int rx)
+    {
+        val += (rx - lx + 1) * v;
+        lazy += v;
+        isLazy = true;
+    }
+};
+
+class Segment_Tree
+{
+private:
+    int size;
+    vector<Node> tree;
+    void init(const vector<int> &nums, int id, int lx, int rx)
+    {
+        if (lx == rx)
+        {
+            if (lx <= sz(nums))
+                tree[id] = Node(nums[lx - 1]);
+            return;
+        }
+
+        int mid = (lx + rx) / 2;
+        init(nums, 2 * id, lx, mid);
+        init(nums, 2 * id + 1, mid + 1, rx);
+
+        tree[id] = merge(tree[2 * id], tree[2 * id + 1]);
+    }
+
+    Node merge(Node &l, Node &r)
+    {
+        return Node(l.val + r.val);
+    }
+
+    void propagate(int lx, int rx, int id)
+    {
+        if (lx == rx || !tree[id].isLazy)
+            return;
+
+        int mid = (lx + rx) / 2;
+        tree[2 * id].update(tree[id].lazy, lx, mid);
+        tree[2 * id + 1].update(tree[id].lazy, mid + 1, rx);
+
+        tree[id].lazy = tree[id].isLazy = 0;
+    }
+
+    void update(const int l, const int r, const int v, int id, int lx, int rx)
+    {
+        propagate(lx, rx, id);
+
+        if (l > rx || r < lx)
+            return;
+
+        if (l <= lx && r >= rx)
+        {
+            tree[id].update(v, lx, rx);
+            return;
+        }
+
+        int mid = (lx + rx) / 2;
+        update(l, r, v, 2 * id, lx, mid);
+        update(l, r, v, 2 * id + 1, mid + 1, rx);
+
+        tree[id] = merge(tree[2 * id], tree[2 * id + 1]);
+    }
+
+    Node get(const int l, const int r, int id, int lx, int rx)
+    {
+        propagate(lx, rx, id);
+
+        if (l > rx || r < lx)
+            return Node();
+        if (l <= lx && r >= rx)
+        {
+            return tree[id];
+        }
+
+        int mid = (lx + rx) / 2;
+        Node ln = get(l, r, 2 * id, lx, mid);
+        Node rn = get(l, r, 2 * id + 1, mid + 1, rx);
+
+        return merge(ln, rn);
+    }
+
+public:
+    Segment_Tree(int n)
+    {
+        size = 1;
+        while (size < n)
+            size <<= 1;
+        tree = vector<Node>(2 * size, Node());
+    }
+
+    void init(const vector<int> &nums)
+    {
+        init(nums, 1, 1, size);
+    }
+
+    void update(const int l, const int r, const int v)
+    {
+        update(l, r, v, 1, 1, size);
+    }
+
+    int get(const int l, const int r)
+    {
+        return get(l, r, 1, 1, size).val;
+    }
+};
+
+int main()
+{
+    return 0;
+}
